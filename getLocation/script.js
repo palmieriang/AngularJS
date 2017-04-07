f939abdb0f9386077d187d1f5442b3e78ffe9968
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-07 15:33:41 +0100

    get distance

--- getLocation/script.js
+++ getLocation/script.js
@@ -58,5 +58,24 @@ myApp.controller("myController", function($scope) {
 
 		return str;
 	}
+
+	function getDistanceFromLatLonIn(lat1,lon1,lat2,lon2) {
+		var R = 3959; // Radius of the earth in mi
+		var dLat = deg2rad(lat2-lat1);  // deg2rad below
+		var dLon = deg2rad(lon2-lon1); 
+		var a = 
+			Math.sin(dLat/2) * Math.sin(dLat/2) +
+			Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+			Math.sin(dLon/2) * Math.sin(dLon/2)
+			; 
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		var distance = R * c; // Distance in mi
+		// console.log(distance);
+		return distance;
+	}
+
+	function deg2rad(deg) {
+		return deg * (Math.PI/180);
+	}
   
 });
