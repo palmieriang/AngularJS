28c0003e3024904bd17dd0e63fd2e2f705bf6c60
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-07 15:33:00 +0100

    getLocation

--- getLocation/script.js
+++ getLocation/script.js
@@ -2,6 +2,61 @@ var myApp = angular.module("myModule", []);
 
 myApp.controller("myController", function($scope) {
 
-	$scope.test = "test";
+	lat2 = 0;
+	lon2 = 0;
+
+	getLocation();
+
+	function getLocation()
+	{
+		$scope.loaderText = "Getting your location";
+		$scope.loading = true;
+		if(navigator.geolocation)
+		{
+			setTimeout(function()
+			{
+				if($scope.clientPosition.latitude == 0)
+				{
+					console.log('Location ignored');
+				}
+			}, 11000);
+			navigator.geolocation.getCurrentPosition(geolocationSuccess);
+		}
+		else
+		{
+			console.log("Location Unavailable");
+		}
+	}
+
+	function geolocationSuccess(pos)
+	{
+		$scope.loaderText = " ";
+		$scope.geolocated = true;
+		$scope.clientPosition = pos.coords;
+		$scope.sortResultsSelect = 'distance';
+		search();
+	};
+
+	function search()
+	{
+		// alert("caller is " + arguments.callee.caller.toString());
+		$scope.loading = true;
+		var latitude = ($scope.clientPosition ? $scope.clientPosition.latitude || 0 : 0);
+		var longitude = ($scope.clientPosition ? $scope.clientPosition.longitude || 0 : 0);
+		// console.log(latitude);
+		// console.log(longitude);
+		$scope.distance = getDistanceFromLatLonIn(latitude,longitude,lat2,lon2);
+		$scope.$apply();
+	}
+
+	function undToStr(str)
+	{
+		if(angular.isUndefined(str))
+		{
+			str = '';
+		}
+
+		return str;
+	}
   
 });
