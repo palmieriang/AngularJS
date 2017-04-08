90057704394876307cff2999cc2d2e303c5808b4
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-08 18:16:18 +0100

    new function

--- getLocation/script.js
+++ getLocation/script.js
@@ -5,74 +5,25 @@ myApp.controller("myController", function($scope) {
 	lat2 = 0;
 	lon2 = 0;
 	
-	// nearme();
-	// function nearme() {
-	//     if (navigator.geolocation) {
-	//         navigator.geolocation.getCurrentPosition(function (position) {
-
-	//                 var latitude = position.coords.latitude; 
-	//                 var longitude = position.coords.longitude;
-	//                 console.log(latitude);
-	//                 console.log(longitude);
-	// 				$scope.distance = getDistanceFromLatLonIn(latitude,longitude,lat2,lon2);
-	// 				$scope.$apply();
-	//         });
-	        
-	//     }
-	// }
-
 	getLocation();
-
-	function getLocation()
-	{
+	function getLocation() {
 		$scope.loaderText = "Getting your location";
-		$scope.loading = true;
-		if(navigator.geolocation)
-		{
-			setTimeout(function()
-			{
-				if($scope.clientPosition.latitude == 0)
-				{
-					console.log('Location ignored');
-				}
-			}, 11000);
-			navigator.geolocation.getCurrentPosition(geolocationSuccess);
-		}
-		else
-		{
-			console.log("Location Unavailable");
-		}
-	}
-
-	function geolocationSuccess(pos)
-	{
-		$scope.loaderText = " ";
-		$scope.geolocated = true;
-		$scope.clientPosition = pos.coords;
-		$scope.sortResultsSelect = 'distance';
-		search();
-	};
-
-	function search()
-	{
-		// alert("caller is " + arguments.callee.caller.toString());
-		$scope.loading = true;
-		var latitude = ($scope.clientPosition ? $scope.clientPosition.latitude || 0 : 0);
-		var longitude = ($scope.clientPosition ? $scope.clientPosition.longitude || 0 : 0);
-		// console.log(latitude);
-		// console.log(longitude);
-		$scope.distance = getDistanceFromLatLonIn(latitude,longitude,lat2,lon2);
-		$scope.$apply();
-	}
-
-	function undToStr(str)
-	{
-		if(angular.isUndefined(str))
-		{
-			str = '';
-		}
-
-		return str;
+		$scope.loading = true;		
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(function (position) {
+					$scope.loaderText = " ";
+					$scope.geolocated = true;
+	                var latitude = position.coords.latitude; 
+	                var longitude = position.coords.longitude;
+	                // console.log(latitude);
+	                // console.log(longitude);
+					$scope.distance = getDistanceFromLatLonIn(latitude,longitude,lat2,lon2);
+					$scope.$apply();
+	        });
+	        
+	    } else {
+			$scope.loaderText = "Location Unavailable";
+	    }
 	}
 
 	function getDistanceFromLatLonIn(lat1,lon1,lat2,lon2) {
