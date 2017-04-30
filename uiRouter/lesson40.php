64af4e39079911bd8222acd38e24e2904dbf0720
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-30 19:29:06 +0100

    ui-router

---
+++ uiRouter/lesson40.php
@@ -0,0 +1,36 @@
+<?php
+
+	header('Access-Control-Allow-Origin: *');  
+
+	$db_host = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "databaseTest";
+
+    $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name) or die("Some error occurred during connection " . mysqli_error($con));
+
+	$sql = "SELECT * FROM employees WHERE name LIKE '%" . $_GET["name"] . "%'";
+    
+    $result = mysqli_query($conn, $sql);
+    if($result)
+    {
+        $employees = array();
+    	while($row = mysqli_fetch_assoc($result))
+    	{
+            $employee = array();
+            $employee['ID'] = $row['ID'];
+            $employee['name'] = $row['name'];
+            $employee['gender'] = $row['gender'];
+            $employee['salary'] = $row['salary'];
+            $employees[] = $employee;
+    	}
+
+        echo json_encode($employees);
+
+    }
+    else
+    {
+    	echo mysqli_error($conn);
+    }
+
+?>
---
+++ uiRouter/script.js
@@ -0,0 +1,162 @@
+var app = angular.module("Demo", ["ui.router"])
+				 .config(function($stateProvider, $urlMatcherFactoryProvider, $urlRouterProvider, $locationProvider) {
+				 	$urlRouterProvider.otherwise("/home");
+				 	$urlMatcherFactoryProvider.caseInsensitive(true);
+				 	$stateProvider
+				 		.state("home", {
+				 			url: "/home",
+				 			templateUrl: "exercises/angular.3.UiRouter/templates/home.html",
+				 			controller: "homeController",
+				 			data: {
+				 				customData1: "Home State Custom Data 1",
+				 				customData2: "Home State Custom Data 2"
+				 			}
+				 		})
+				 		.state("courses", {
+				 			url: "/courses",
+				 			templateUrl: "exercises/angular.3.UiRouter/templates/courses.html",
+				 			controller: "coursesController",
+				 			data: {
+				 				customData1: "Courses State Custom Data 1",
+				 				customData2: "Courses State Custom Data 2"
+				 			}
+				 		})
+				 		.state("studentParent", {
+				 			url: "/students",
+				 			templateUrl: "exercises/angular.3.UiRouter/templates/studentParent.html",
+				 			controller: "studentParentController",
+				 			resolve: {
+				 				studentTotals: function($http) {
+				 					return $http.get("http://localhost/exercises/angular.3.UiRouter/lesson25.php")
+				 							.then(function(response) {
+				 								return response.data;
+				 							})
+				 				}
+				 			},
+				 			abstract: true
+				 		})
+				 		.state("studentParent.students", {
+				 			url: "/",
+				 			views: {
+				 				"studentData": {
+						 			templateUrl: "exercises/angular.3.UiRouter/templates/students.html",
+						 			controller: "studentsController",
+						 			resolve: {
+						 				studentsList: function($http) {
+						 					return $http.get("http://localhost/exercises/angular.3.UiRouter/lesson25.php")
+						 							.then(function(response) {
+						 								return response.data;
+						 							})
+						 				}
+						 			}
+						 		},
+						 		"totalData": {
+						 			templateUrl: "exercises/angular.3.UiRouter/templates/studentsTotal.html",
+						 			controller: "studentsTotalController"
+						 		}
+
+				 			}
+				 		})
+				 		.state("studentParent.studentDetails", {
+				 			url: "/:ID",
+				 			views: {
+				 				studentData: {
+						 			templateUrl: "exercises/angular.3.UiRouter/templates/studentDetails.html",
+						 			controller: "studentDetailsController"				 					
+				 				}
+				 			},
+				 		})
+				 		.state("studentsSearch", {
+				 			// we don't need the question mark to make it optional anymore
+				 			url: "/:name",
+				 			templateUrl: "exercises/angular.3.UiRouter/templates/studentsSearch.html",
+				 			controller: "studentsSearchController"
+				 		})
+				 		.state("test", {
+				 			url: "/test",
+				 			template: "<h1>Inline template in action</h1>",
+				 			controller: "homeController"
+				 		})
+
+			 		// $locationProvider.html5Mode(true);
+				 })
+				.controller("studentsTotalController", function ($scope, studentTotals) {
+				 	$scope.students = studentTotals;
+
+					$scope.total = $scope.students.length;
+				})
+				.controller("studentParentController", function ($scope, studentTotals) {
+				 	$scope.students = studentTotals;
+
+					$scope.total = $scope.students.length;
+					$scope.totalMale = 0;
+					$scope.totalFemale = 0;
+
+					for(var i=0; i<$scope.students.length; i++) {
+						if($scope.students[i].gender == "Male") {
+							$scope.totalMale += 1;
+						} else {
+							$scope.totalFemale += 1;
+						}
+					}
+				})
+				 .controller("homeController", function($scope, $state) {
+				 	$scope.message = "Home Page";
+
+				 	$scope.homeCustomData1 = $state.current.data.customData1;
+				 	$scope.homeCustomData2 = $state.current.data.customData2;
+
+				 	$scope.coursesCustomData1 = $state.get("courses").data.customData1;
+				 	$scope.coursesCustomData2 = $state.get("courses").data.customData2;
+				 })
+				 .controller("coursesController", function($scope) {
+				 	$scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
+				 })
+				 .controller("studentsController", function(studentsList, $scope, $http, $state, $stateParams, $location, studentTotals) {
+
+				 	$scope.searchStudent = function() {
+				 		$state.go("studentsSearch", { name: $scope.name });
+				 	}
+
+				 	$scope.reloadData = function() {
+				 		$state.reload();
+				 	}
+
+				 	$scope.students = studentsList;
+				 	$scope.total = studentTotals.length;
+
+				 	// $scope.searchStudent = function(name) {
+				 	// 	if($scope.name) {
+				 	// 		$location.url("/studentsSearch/" + $scope.name);
+				 	// 	} else {
+				 	// 		$location.url("/studentsSearch");
+				 	// 	}
+				 	// 	$http.get("http://localhost/exercises/angular.3.UiRouter/lesson40.php?name="+$stateParams.name)
+				 	// 	 .then(function(response) {
+						// 	$scope.students = response.data;
+				 	// 	 })
+				 	// }
+				 })
+				.controller("studentDetailsController", function ($scope, $http, $stateParams) {
+					$http.get("http://localhost/exercises/angular.3.UiRouter/lesson30.php?ID="+$stateParams.ID)
+				 		 .then(function(response) {
+				    	console.log(response);
+							$scope.student = response.data[0];
+				 		 })
+				})
+				.controller("studentsSearchController", function ($scope, $http, $stateParams) {
+				    if ($stateParams.name) {
+				        $http({
+				            url: "http://localhost/exercises/angular.3.UiRouter/lesson40.php",
+				            method: "get",
+				            params: { name: $stateParams.name }
+				        }).then(function (response) {
+				            $scope.students = response.data;
+				        })
+				    } else {
+				        $http.get("http://localhost/exercises/angular.3.UiRouter/lesson25.php")
+								.then(function (response) {
+								    $scope.students = response.data;
+								})
+				    }
+				})
