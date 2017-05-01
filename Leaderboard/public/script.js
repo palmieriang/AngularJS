649e275f8c24472c47d39b0968a4c18ef49c122f
Angelo Palmieri <palmieri.ang@gmail.com>
2017-05-01 22:36:16 +0100

    sass

--- Leaderboard/public/script.js
+++ Leaderboard/public/script.js
@@ -7,4 +7,20 @@ myApp.controller("myController", function($scope, $http) {
 			$scope.campers = response.data;
 	})
 
+	$scope.sortColumn = "name";
+
+	$scope.reverseSort = false;
+
+	$scope.sortData = function (column) {
+		$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+		$scope.sortColumn = column;
+	}
+
+	$scope.getSortClass = function(column) {
+		if($scope.sortColumn == column) {
+			return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+		}
+		return '';
+	}
+
 });
