e70e5283b9acd023cba2e3b7b559c68b469c8e20
Angelo Palmieri <palmieri.ang@gmail.com>
2017-06-11 19:43:31 +0100

    edit task

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,17 +1,27 @@
 angular.module('myModule', [])
-	.controller('myController', function($scope) {
+.controller('myController', function($scope) {
 
-		$scope.tasks = [];
+	$scope.tasks = [];
 
-		$scope.searchEnter = function() {
-			if(event.which == 13 && $scope.task != '') {
-				$scope.addTask();
-			};
+	$scope.searchEnter = function() {
+		if(event.which == 13 && $scope.task != '') {
+			$scope.addTask();
 		};
+	};
 
-		$scope.addTask = function() {
-			$scope.tasks.push({'taskMessage': $scope.task, 'status': false});
-			console.log($scope.tasks);
-			$scope.task = ' ';
+	$scope.addTask = function() {
+		$scope.tasks.push({'taskMessage': $scope.task, 'status': false});
+		console.log($scope.tasks);
+		$scope.task = ' ';
+	};
+
+	$scope.editTask = function() {
+		event.target.contentEditable = event.target.contentEditable == "false" ? "true" : "false";
+	};
+
+	$scope.enterAgain = function(task) {
+		if(event.which == 13 && task != '') {
+			$scope.editTask();
 		};
-	});
+	};
+});
